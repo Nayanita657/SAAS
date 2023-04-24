d4d5bc0eb727b0509f2288c3956328d00aa13544
Nayanita657 <93154897+Nayanita657@users.noreply.github.com>
2023-04-24 11:56:30 +0530

    Update MainPage.cs

--- Sensor Client/MainPage.cs
+++ Sensor Client/MainPage.cs
@@ -25,6 +25,8 @@ public class MainPage : ContentPage
 		if (File.Exists(_idFile))
 			ID = byte.Parse(File.ReadAllText(_idFile));
 		_runningTask = null!;
+		else
+			ID++;
 		Content = new ScrollView()
 		{
 			Content = new VerticalStackLayout()
